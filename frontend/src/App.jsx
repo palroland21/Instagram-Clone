a319b67d5edf41f145a0cf669829bf8fc29e699a
Vasi Breban <vbreban09@gmail.com>
2026-04-23 11:36:58 +0300

    search page implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import AuthPage from './pages/AuthPage'
 import HomePage from './pages/HomePage'
 import ProfilePage from "./pages/ProfilePage.jsx";
+import SearchPage from "./pages/SearchPage.jsx";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <Route path="/home" element={<HomePage />} />
             <Route path="/profile" element={<ProfilePage />} />
             <Route path="*" element={<Navigate to="/" replace />} />
+            <Route path="/search" element={<SearchPage />} />
         </Routes>
     )
 }
